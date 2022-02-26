257a49c49140d4999a1c751a350491f49e977bde
Gerson1013 <98921943+Gerson1013@users.noreply.github.com>
2022-02-26 15:51:28 -0400

    Add files via upload

---
+++ Mi primer programa.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//Mi primer programa :D
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+float Number1, Number2;
+
+cout<<"digite el numero 1: "; cin>>Number1;
+cout<<"digite el numero 2: "; cin>>Number2;
+
+cout<<"suma:"<<(Number1+Number2)<<endl;
+cout<<"multiplicacion: "<<(Number1*Number2)<<endl;
+cout<<"division: "<<(Number1/Number2)<<endl;
+cout<<"resta: "<<(Number1-Number2)<<endl;
+    
+    return 0;
+}
